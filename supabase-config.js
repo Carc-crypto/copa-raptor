64d19cdd71d6a44e3f0aba34d1ab921dc47fcbd2
Cristopher Ramos <tcris6496@gmail.com>
2026-09-23 13:45:09 -0600

    pimini

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,4 @@
 const SUPABASE_URL = "https://gigcjdhnnjnrojentaev.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_gBHOBIn8vmrgFWZdT0cgVg_prdJJmNC"; 
+const SUPABASE_KEY = "sb_publishable_gBHOBIn8vmrgFWZdT0cgVg_prdJJmNC";
 
-const supabaseClient = window.supabase 
-  ? window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY) 
-  : null;
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
