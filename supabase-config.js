594892f32617c3885553853217ca0a073dba2287
Cristopher Ramos <tcris6496@gmail.com>
2026-09-23 11:56:50 -0600

    probkema inicio de sesion

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,6 @@
 const SUPABASE_URL = "https://gigcjdhnnjnrojentaev.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_gBHOBIn8vmrgFWZdT0cgVg_prdJJmNC"; 
 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = window.supabase 
+  ? window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY) 
+  : null;
