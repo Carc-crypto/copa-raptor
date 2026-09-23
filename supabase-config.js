65ada2affb4e5abc6c446ef703c96f8a28b146c6
Cristopher Ramos <tcris6496@gmail.com>
2026-09-23 13:53:26 -0600

    nbnbnhbh

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://gigcjdhnnjnrojentaev.supabase.co";
-const SUPABASE_KEY = "sb_publishable_gBHOBIn8vmrgFWZdT0cgVg_prdJJmNC";
+const SUPABASE_ANON_KEY = "sb_publishable_gBHOBIn8vmrgFWZdT0cgVg_prdJJmNC";
 
 const supabaseClient = window.supabase 
-  ? window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY) 
+  ? window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY) 
   : null;
