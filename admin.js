ba8b6e0412d09c28cde49ccacd245dd917a69256
Cristopher Ramos <tcris6496@gmail.com>
2026-09-24 09:26:47 -0600

    jujujuj

--- admin.js
+++ admin.js
@@ -396,12 +396,8 @@ document.addEventListener("DOMContentLoaded", checkSession);
 } else {
 checkSession();
 }
-document.addEventListener("DOMContentLoaded", () => {
-  const rootElement = document.getElementById("tu-id-contenedor"); // Reemplaza por el ID correspondiente
+document.addEventListener("DOMContentLoaded", () => {const rootElement = document.getElementById("tu-id-contenedor"); // Reemplaza por el ID correspondiente
 
-  if (rootElement) {
-    // Código que inicializa el componente usando rootElement
-  } else {
-    console.error("El elemento raíz no existe en esta página.");
+  if (rootElement) {} else {console.error("El elemento raíz no existe en esta página.");
   }
 });
